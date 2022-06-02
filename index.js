4830dddaf05ea71fab2d4e172979ac2b611e1a85
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-02 15:08:00 +0300

    done assignement

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToHeadphonedRoommate(string) {
     if (string === string.toLowerCase()) {
         return "I can\'t hear you!"
     } else if (string === string.toUpperCase()) {
-        return "YES INDEED!""'
+        return "YES INDEED!"
     } else if (string === "Let\'s have dinner together!")
         return "I would love to!"
 
